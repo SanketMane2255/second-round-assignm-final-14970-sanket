d64516efa58c4812cdf2b8a25e57b6e5929cfe01
Sanket Mane <sanketmane2255@gmail.com>
2026-03-31 12:22:15 +0530

    React project creation & dependencies installations.

---
+++ chatbox_app/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className="text-3xl font-bold ">Chatbox App</h1>
+      <p>Welcome to the Chatbox App! This is a simple application where you can chat with your friends and family. You can create chat rooms, send messages, and share media files. Enjoy chatting!</p>
+
+    </>
+  )
+}
+
+export default App
